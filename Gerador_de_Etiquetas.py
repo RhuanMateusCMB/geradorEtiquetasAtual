f4c870dc6675e54583f8faacd4a6ca5898862c23
RhuanMateusCMB <rhuanmateuscmb@gmail.com>
2025-01-09 15:16:18 -0300

    Update Gerador_de_Etiquetas.py

--- Gerador_de_Etiquetas.py
+++ Gerador_de_Etiquetas.py
@@ -52,6 +52,8 @@ def extrair_itens_pedido(conteudo_pdf, pacote_dict, nome_dict):
                     # Agora pegamos o código do produto do primeiro grupo
                     id_produto = str(int(match_completo.group(1)))
                     quantidade_str = match_completo.group(3)
+                    # Remover pontos antes de converter para float
+                    quantidade_produto = float(quantidade_str.replace('.', '').replace(',', '.'))
                     unidade = match_completo.group(4).upper() if match_completo.group(4) else 'UN'
                 elif match_sem_nome:
                     id_produto = str(int(match_sem_nome.group(1)))
